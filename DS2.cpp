72648e970b910b150d0018c4877db0c6fdd5dca7
Ahmed <ahmed.morsy1995@gmail.com>
2018-05-12 12:08:29 +0200

    uffffFFF

--- DS2.cpp
+++ DS2.cpp
@@ -133,20 +133,182 @@ bool search(node *now,int key){
    if(key<now->value)return search(now->left,key);
    return true;
 }
-int getLevel(node *now){
-   if(now==NULL)return 0;
-   return 1+max(getLevel(now->left),getLevel(now->right));
+node* successor(node *p)
+{
+      node *y=NULL;
+     if(p->left!=NULL)
+     {
+         y=p->left;
+         while(y->right!=NULL)
+              y=y->right;
+     }
+     else
+     {
+         y=p->right;
+         while(y->left!=NULL)
+              y=y->left;
+     }
+     return y;
 }
-int getSize(node *now){
-   if(now==NULL)return 0;
-   return 1 + getSize(now->left)+getSize(now->right);
+void fixMyDelete(node *p)
+{
+    node *s;
+    ///case1 en node de tb2a root f msh ma7tga a3ml 7aga asln f msh 7yd5ol f while
+    while(p!=root&&p->color=='b')
+    {
+          if(p->parent->left==p)
+          { ///b7dad sibling
+                  s=p->parent->right;
+                  ///case2
+                  if(s->color=='r')
+                  {    ///b3ml leftRotation lal parent w aswap el colors
+                         s->color='b';
+                         p->parent->color='r';
+                         leftRotate(p->parent);
+                         s=p->parent->right;
+                  }
+                  ///case3 75li sibling red w abdl el node bt3ty b parent
+                  if(s->right->color=='b'&&s->left->color=='b')
+                  {
+                         s->color='r';
+                         p=p->parent;
+                  }
+                  else
+                  {
+                      if(s->right->color=='b')
+                      {
+                             s->left->color='b';
+                             s->color='r';
+                             rightRotate(s);
+                             s=p->parent->right;
+                      }
+                      s->color=p->parent->color;
+                      p->parent->color='b';
+                      s->right->color='b';
+                      leftRotate(p->parent);
+                      p=root;
+                  }
+          }
+          else
+          {
+                  s=p->parent->left;
+                  if(s->color=='r')
+                  {
+                        s->color='b';
+                        p->parent->color='r';
+                        rightRotate(p->parent);
+                        s=p->parent->left;
+                  }
+                  if(s->left->color=='b'&&s->right->color=='b')
+                  {
+                        s->color='r';
+                        p=p->parent;
+                  }
+                  else
+                  {
+                        if(s->left->color=='b')
+                        {
+                              s->right->color='b';
+                              s->color='r';
+                              leftRotate(s);
+                              s=p->parent->left;
+                        }
+                        s->color=p->parent->color;
+                        p->parent->color='b';
+                        s->left->color='b';
+                        rightRotate(p->parent);
+                        p=root;
+                  }
+          }
+       p->color='b';
+       root->color='b';
+    }
+}
+void displayForDel(node *DELNODE){
+    cout<<"Deleted Element:\n "<<DELNODE->value;
+         cout<<"Colour: ";
+         if(DELNODE->color=='B')
+     cout<<"Black\n";
+    else
+     cout<<"Red\n";
+     if(DELNODE->parent!=NULL)
+               cout<<"Parent:\n"<<DELNODE->parent->value;
+         else
+               cout<<"There is no parent of the node this node is the root.\n";
+         if(DELNODE->right!=NULL)
+               cout<<"Right Child:\n"<<DELNODE->right->value;
+         else
+               cout<<"There is no right child of the node.\n";
+         if(DELNODE->left!=NULL)
+               cout<<"Left Child:\n "<<DELNODE->left->value;
+         else
+               cout<<"There is no left child of the node.\n ";
+         cout<<"DONE\n";
+}
+void deleteNode(int x){
+    if(root==NULL)
+        cout<<"Error: Empty Tree \n";
+       node *DELNODE;
+       DELNODE=root;
+        node *y=NULL;
+        node *q=NULL;
+        int found =0;
+        ///momkn nandy 3la function search ashl
+        while(DELNODE!=NULL&&found==0)
+     {
+           if(DELNODE->value==x)
+               found=1;
+           if(found==0)
+           {
+                 if(DELNODE->value<x)
+                    DELNODE=DELNODE->right;
+                 else
+                    DELNODE=DELNODE->left;
+           }
+     }
+      if(found==0)
+     {
+            cout<<"Element Not Found.\n";
+            return ;
+     }
+     else
+     {
+         displayForDel(DELNODE);
+     if(DELNODE->left==NULL||DELNODE->right==NULL)
+              y=DELNODE;
+     else
+        y=successor(DELNODE);
+         if(y->left!=NULL)
+              q=y->left;
+         else
+         {
+              if(y->right!=NULL)
+                   q=y->right;
+              else
+                   q=NULL;
+         }
+         if(q!=NULL)
+              q->parent=y->parent;
+         if(y->parent==NULL)
+              root=q;
+         else
+         {
+             if(y==y->parent->left)
+                y->parent->left=q;
+             else
+                y->parent->right=q;
+         }
+         if(y!=DELNODE)
+         {
+              DELNODE->color=y->color;
+             DELNODE->value=y->value;
+         }
+         if(y->color=='b')
+             fixMyDelete(q);
+     }
 }
 int main()
 {
-    for(int i=0;i<16;i++){
-        insertRB(i);
-        cout<<getSize(root)<<"    "<<getLevel(root)<<endl;
-    }
+
     return 0;
 }
-
